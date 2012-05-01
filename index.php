7ce3efa6f914e1e12c8e13d2cd6fe78dd30460ae
Tom Ritchford <tom@swirly.com>
2012-05-01 15:08:00 -0400

    Fix the author's name

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!-- DW6 -->
 <head>
 <!-- Copyright 2005 Macromedia, Inc. All rights reserved. -->
-<title>Petition Script PHP</title>
+<title>Drone Pledge</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel="stylesheet" href="phpPetitionTemplate/mm_travel2.css" type="text/css">
 <script language="javascript">
@@ -17,14 +17,16 @@ var TODAY = monthname[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear()
 <body bgcolor="#C0DFFD">
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
   <tr bgcolor="#3366CC">
-    <td width="382" colspan="3" rowspan="2"><img src="phpPetitionTemplate/mm_travel_photo.jpg" alt="Petition Script PHP" width="382" height="127" border="0"></td>
-    <td width="378" height="63" colspan="3" id="logo" valign="bottom" align="center" nowrap>Petition Script PHP  </td>
+    <td width="382" colspan="3" rowspan="2">
+      <img src="http://www.icrac.co.uk/Swat%20bkg.jpg" alt="Drone Pledge" width="382" height="127" border="0">
+    </td>
+    <td width="378" height="63" colspan="3" id="logo" valign="bottom" align="center" nowrap>Drone Pledge  </td>
     <td width="100%">&nbsp;</td>
   </tr>
 
   <tr bgcolor="#3366CC">
-    <td height="64" colspan="3" id="tagline" valign="top" align="center">Where Your Opinion Counts </td>
-	<td width="100%">&nbsp;</td>
+    <td height="64" colspan="3" id="tagline" valign="top" align="center">I Pledge Not To Make Drones </td>
+ <td width="100%">&nbsp;</td>
   </tr>
 
   <tr>
@@ -32,13 +34,12 @@ var TODAY = monthname[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear()
   </tr>
 
   <tr valign="middle" bgcolor="#CCFF99">
-  	<td colspan="1" id="dateformat" height="25">&nbsp;&nbsp;
-  	  <script language="javascript">
-      document.write(TODAY);</script>	</td>
+   <td colspan="1" id="dateformat" height="25">&nbsp;&nbsp;
+     <script language="javascript"> document.write(TODAY);</script> </td>
     <td colspan="5" id="dateformat" height="25" valign="middle">
-	<a href="http://www.chimicon.com/petition/"><img src="http://petitionscript.net/Images/ASPpetition.gif" alt="Petition Script Available in ASP" border="0" align="right"></a>	</td>
-	<td id="dateformat" height="25" valign="middle">
-	</td>
+ <a href="http://www.icrac.co.uk/">Sponsored by ICRAC</a> </td>
+ <td id="dateformat" height="25" valign="middle">
+ </td>
   </tr>
  <tr>
     <td colspan="7" bgcolor="#003366"><img src="phpPetitionTemplate/mm_spacer.gif" alt="" width="1" height="1" border="0"></td>
@@ -46,7 +47,7 @@ var TODAY = monthname[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear()
 
  <tr>
     <td width="165" valign="top" bgcolor="#E6F3FF">
-	<table border="0" cellspacing="0" cellpadding="0" width="165" id="navigation">
+ <table border="0" cellspacing="0" cellpadding="0" width="165" id="navigation">
         <tr>
           <td width="165"><a href="Referral/demo.php" class="navText">Refer A Friend </a></td>
         </tr>
@@ -56,23 +57,11 @@ var TODAY = monthname[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear()
         <tr>
           <td width="165"><a href="http://petitionscript.net/phpSupport/index.php" class="navText">Support </a></td>
         </tr>
-		<tr>
-		<td><A HREF="http://www.scriptsearch.com">ScriptSearch.com</A></td>
-		</tr>
-		<tr>
-		<td><A HREF="Admin/Index.php">ADMIN LOGIN </A></td>
-		</tr>
+  <tr>
+  <td><A HREF="Admin/Index.php">ADMIN LOGIN </A></td>
+  </tr>
       </table>
- 	 
- 	<div align="center">&nbsp;<img src="http://petitionscript.net/phpPETITION2/phpPetitionTemplate/php.gif" alt="Petition Script PHP" width="123" height="110" longdesc="http://thewebsiteguru.com"><br>
-        <a href="phpPETITION.zip">CLICK TO<br>
- 	    DOWNLOAD NOW</a>
-        <p><br>
- 	      <a href="../CFpetition/"><img src="http://petitionscript.net/phpPETITION2/admin/images/logo_phpbb.gif" alt="Petition Script" width="200" height="91" border="0"></a><br>
- 	      <a href="http://petitionscript.net/CFpetition/default.cfm">Also Available in <br>
- 	    Cold Fusion</a></p>
- 	  <p class="subHeader"><br></p>
- 	</div>
+  </div>
 <style type="text/css">
 <!--
 .title {
@@ -84,50 +73,69 @@ text-decoration: none;
 }
 -->
 </style>
-<table width="220" cellpadding="1" cellspacing="1" bgcolor="#FF6600"><form action="http://www.scripts.com/rate.php" method="POST"><input type="hidden" name="ID" value="23315"> <tr>  <td><table width="220" border="0" cellpadding="3" cellspacing="3" bgcolor="#FF9900"> <tr>  <td width="45"><a href="http://www.scripts.com"><img src="http://images.devshed.com/scr/misc/logo_01.gif" width="45" height="28" border="0"></a></td> <td width="164"><a href="http://www.scripts.com" class="title">Scripts.com</a></td> </tr> <tr bgcolor="#FFFFFF">  <td colspan="2"><center> <font size="2" face="Arial, Helvetica, sans-serif"><b>Rate  Our Program!</b></font>  </center> <center> </center> <center> <input type="radio" name="rating" value="5"> <font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><br> <input type="radio" name="rating" value="4"> <font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_0.gif" width="15" height="15"></font><br> <input type="radio" name="rating" value="3"> <font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_0.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_0.gif" width="15" height="15"></font><br> <input type="radio" name="rating" value="2"> <font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_0.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_0.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_0.gif" width="15" height="15"></font><br> <input type="radio" name="rating" value="1"> <font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_1.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_0.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_0.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_0.gif" width="15" height="15"></font><font size="2" face="Arial, Helvetica, sans-serif"><img src="http://images.devshed.com/scr/misc/star_0.gif" width="15" height="15"></font>  </center> <center> <input name="vote3" type="submit" id="vote3" value="Vote!"> </center></td> </tr> </table></td> </tr> </form> </table> 
   </td>
     <td width="50"><img src="phpPetitionTemplate/mm_spacer.gif" alt="" width="50" height="1" border="0"></td>
     <td width="305" colspan="2" valign="top"><img src="phpPetitionTemplate/mm_spacer.gif" alt="" width="305" height="1" border="0"><br>
-	&nbsp;<br>
-	&nbsp;<br>
-	<table border="0" cellspacing="0" cellpadding="0" width="305">
+ &nbsp;<br>
+ &nbsp;<br>
+ <table border="0" cellspacing="0" cellpadding="0" width="305">
         <tr>
-          <td class="pageName">Welcome To Petition Script </td>
-		</tr>
+          <td class="pageName">The Pledge Against Making Drones. </td>
+  </tr>
 
-		<tr>
-          <td class="bodyText"><p>This Home Page layout makes a great starting point for your Petition. Virtually all of the content is customizable, including the images, the text, and the links. You can decide whether to keep the existing graphics or swap them out for pictures of your own.</p>
+  <tr>
+          <td class="bodyText"><p>Drones are a force of great evil.</p>
 
-		    <p>The text on this page is intended to help you jumpstart your design by suggesting the sort of content you may want to include, but don't let it limit you. The same is also true for the link text - feel free to change the names of the links to better suit your particular needs. The Database and Fields can be expanded as well... Have fun and make a great Petition website! (Customizations and Installation Available; starting at US Funds $25.00 <a href="mailto:cs@thewebsiteguru.com">email</a><a href="mailto:cs@thewebsiteuru.com"></a> us for details) </p></td>
+      <p>Here's where you can fight against them.</p></td>
         </tr>
       </table>
-	   <br>
-	  <strong> 	  SIGN OUR PETITION</strong> <br>
+    <br>
+   <strong>    SIGN OUR PETITION</strong> <br>
       <?php 
 #phpinfo();
 include("signature.php"); 
-?>  <p>
-<form class="Content" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-        <p align="center">Now available for the First Time Ever the phpPETITION Customization API.
-<b>Only USD $15.00</b><br>
-<a href="http://www.petitionscript.net/Orders/index.php"><img src="https://www.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0"></a>
-          </p>
-        </form>
-</p>   </td>
+?>  <p/>   </td>
     <td width="50"><img src="phpPetitionTemplate/mm_spacer.gif" alt="" width="50" height="1" border="0"></td>
         <td width="190" valign="top"><br>
-		&nbsp;<br>
-		<table border="0" cellspacing="0" cellpadding="0">
-			<tr>
-			<td colspan="3" class="subHeader" align="center">GET SCRIPTS </td>
-			</tr>
-			<tr>
-			<td id="sidebar" class="smallText" align="center">
-			<a href="http://www.scripts.com">
-			<img src="http://images.devshed.com/scr/banners/banner_160x600.gif" border=0 alt="Scripts.com">			</a>			</td>
-			</tr>
-		</table>		</td>
-	<td width="100%">&nbsp;</td>
+  &nbsp;<br>
+  <table border="0" cellspacing="0" cellpadding="0">
+   <tr>
+   </tr>
+   <tr>
+   <td id="sidebar" class="smallText" align="center">
+<script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
+<script>
+  new TWTR.Widget({
+    version: 2,
+    type: 'search',
+    search: '#drones',
+    interval: 30000,
+    title: '',
+    subject: 'Drones on Twitter',
+    width: 250,
+    height: 300,
+    theme: {
+      shell: {
+        background: '#8ec1da',
+        color: '#ffffff'
+      },
+      tweets: {
+        background: '#ffffff',
+        color: '#444444',
+        links: '#1985b5'
+      }
+    },
+    features: {
+      scrollbar: false,
+      loop: true,
+      live: true,
+      behavior: 'default'
+    }
+  }).render().start();
+  </script></td>
+   </tr>
+  </table>  </td>
+ <td width="100%">&nbsp;</td>
   </tr>
   <tr>
     <td width="165">&nbsp;</td>
@@ -136,7 +144,7 @@ include("signature.php");
     <td width="138">&nbsp;</td>
     <td width="50">&nbsp;</td>
     <td width="190">&nbsp;</td>
-	<td width="100%">&nbsp;</td>
+    <td width="100%">&nbsp;</td>
   </tr>
 </table>
 </body>
